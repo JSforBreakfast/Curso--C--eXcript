f815c602628a2f502f782ffcfe39816f91f7d47a
JSforBreakfast <veslima3@gmail.com>
2019-07-25 17:27:31 -0300

    VARIÁVEIS E TIPOS PRIMITIVOS DO C
    TIPOS PRIMITIVO
    Tipos primitivos são tipos de informação que a linguagem tem implementada. Por exemplo, o tipo numérico, é um tipo de informação que a linguagem C tem implementada em sua definição.
    
    Também podemos entender os tipos primitivos como sendo os tipos de informação mais simples e básicos possíveis. Ou seja, um caractere, um número ou um valor lógico.
    
    Todo sistema computacional faz uso dos tipos primitivos para compor os tipos compostos. Por exemplo, o número de um telefone, é um tipo composto por números e caracteres. Não há razão para a linguagem implementar esse tipo de informação.
    
    A linguagem C tem definido os seguinte tipos primitivos: - char - int - float - void - double
    
    O tipo char O tipo char é um tipo de informação que é capaz de armazenar um caractere alfanumérico. Geralmente, fazemos uso do tipo char para a construção de cadeias de caracteres, ou seja, um conjunto de letras que formam uma frase ou então, uma palavra qualquer.
    
    O tipo int O tipo int representa os números inteiros. Logo, podemos armazenar tanto números positivos como também, números negativos. Normalmente, o tipo int utiliza 2 bytes de memória e assim, é capaz de armazenar números que estejam no intervalo de -32768 até +32767.
    
    O tipo float O tipo float representa os números com ponto flutuante, os números que contenham casas decimais. Normalmente, o tipo float ocupa 4 bytes de memória.
    
    O tipo double O tipo "double" também representa os número com ponto flutuante, porém, conseguimos com o tipo double, muito mais precisão do que temos com o tipo "float". Isso porque, o tipo double faz uso de 8 bytes e assim, consegue armazenar números numa faixa muito extensa.
    
    O tipo void O tipo void representa um tipo sem tipo. Podemos pensar no tipo void como sendo a representação do vazio ou então do nada. Nós normalmente fazemos uso deste tipo em funções que não irão retornar valores. Logo, dissemos que a função retorna void, isto é, não retorna valor algum.
    
    EXEMPLO FEITO EM AULA
    #include <stdio.h>
    
    //  ==========                 ==========
    //  ========== www.eXcript.com ==========
    //  ==========                 ==========
    
    int main()
    {
        /*
            1) char - 1 Byte ou seja, 8 bits
            2) int - 2 Bytes - 32768 + 32767
            3) float - 4 Bytes
            4) void - vazio
            5) double - 8 Bytes,
    
        */
    
        printf( "" );
    
        return 0;
    }

---
+++ VARIÁVEIS E TIPOS PRIMITIVOS DO C.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+/*
+1)char - 1 byte (8bits) caractere ex: vitor
+2)int - 2 bytes (-32768 a 32767) inteiro ex: 30
+3)float - 4 bytes real ex: 2,55
+4)void - vazio
+5)double - 8 bytes (para números com grandes quantidades de casas decimais)
+
+*/
+
+printf("")
+
+return 0;
+
+}
+
