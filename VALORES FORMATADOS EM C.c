9c175aeb6c31fc059266acc8bca8f2b9b7678e4c
JSforBreakfast <veslima3@gmail.com>
2019-07-27 02:04:45 -0300

    OPERADORES ARITMÉTICOS DO C
    TEXTO COM FORMATAÇÕES
    Como imprimir valores formatados na tela? Nessa aula aprenderemos a utilizar a função printf() ou seja, print() vem de imprimir e o f no final, vem de format - imprima formatado.
    
    É importante observar que estamos querendo imprimir valores com ponto flutuante de maneira formatada, ou então, valores do tipo inteiro.
    
    A função printf interpreta alguns conjuntos de caracteres que são precedidos pelo sinal de porcentagem. Por exemplo, %s, diz que o primeiro valor é do tipo string e deve ser concatenado com a string a esquerda do sinal "%s" com o respectivo valor a direita. Da mesma forma temos o %d e %i, que utilizamos para inserir um número inteiro. Já, o %f é utilizado para inserir um valor do tipo ponto flutuante.
    
    Essa é uma maneira bastante conveniente para inserirmos valores dentro de uma cadeia de caracteres. Do contrário, teríamos que trabalhar abrindo e fechado aspas, o que nem sempre é a tarefa mais fácil e poucas vezes, resulta num código legível.
    
    EXEMPLO FEITO EM AULA
    #include <stdio.h>
    
    //  ==========                 ==========
    //  ========== www.eXcript.com ==========
    //  ==========                 ==========
    
    int main()
    {
    //    printf("%tipo_de_saida", valor_a_ser_impresso);
    //    printf("%tipo1 %tipo2", var1, var2);
    
    //    int num = 1111;
    //    printf( "%i\n", num );
    
    //    float f = 2.2;
    //    printf( "%f", f );
    
        //integer %i OU %d
        //float / double %f
        //char %c
        //string %s
    
        //%u módulo de um número
        //%p endereço de memória
        //%e OU %E para imprimir número com notação científica
        //%% para imprimir somente UM sinal de porcentagem
    
        int x = 10;
        printf( "%i\n", x );
    
        float ff = 4.12;
        printf("%f\n", ff);
    
        printf( " %i - %f \n", x, ff );
    
    
        return 0;
    }

---
+++ VALORES FORMATADOS EM C.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+//    printf("%tipo_de_saida", valor_a_ser_impresso);
+//    printf("%tipo1 %tipo2", var1, var2);
+
+//    int num = 1111;
+
+//    printf( "%i\n", num );
+
+//    float f = 2.2;
+//    printf("%f", f)
+
+
+//integer %i ou %d
+//float / double %f
+//char %c
+//string %s
+
+//%u modulo do número
+//%p endereço de memoria
+//%e ou %E números com notação científica
+//%% para imprimir um sinal de porcentagem
+
+int x = 10;
+
+printf("%i\n", x);
+
+float ff = 4.12
+
+printf("%f", ff)
+
+printf("%i - %f ", x, ff)
+
+    return 0;
+}
