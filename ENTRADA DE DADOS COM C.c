052073b952645f07650709ac4d524a2651d2600b
JSforBreakfast <veslima3@gmail.com>
2019-07-28 20:38:08 -0300

    ENTRADA DE DADOS COM C
    ENTRADA DE DADOS
    A entrada de dados em C pode ser feita de diversas maneiras. No prompt, é comum a utilização da função scanf(). Esta, tem por objetivo, devolver todos os caracteres digitados no prompt e retornar para a variável que nós especificamos. Assim, temos a entrada de dados mais primitiva do C, porém, bastante funcional.
    
    Por exemplo:
    
    int valor = 0;
    scanf("%i", &valor);
    A FUNÇÃO scanf()
    O funcionamento da função scanf() é parecido com o funcionamento da função "printf()" que nós já estudamos. A principal diferença é que agora estamos manipulando o fluxo de entrada de dados, enquanto que com a função printf(), estávamos manipulando a saída de informações, ou seja, o Fluxo de Saída.
    
    No primeiro parâmetro da função scanf() temos de informar o tipo de dados que estamos esperando, enquanto que no segundo parâmetro, temos de passar o ponteiro da variável em que desejamos receber o valor oriundo do Fluxo de Entrada de dados.
    
    A seguir, temos uma lista que contém o Tipo do Argumento de entrada de dados que é esperado pela função scanf().
    
    %d e %lg - tipo inteiro ou de ponto flutuante (float) e (double). %i - tipo inteiro %c - caracteres
    
    Há outros valores definidos, porém, nesse momento, devemos conhecer somente estes três tipos.
    
    EXEMPLO FEITO EM AULA
    #include <stdio.h>
    #include <stdlib.h>
    
    //  ==========                 ==========
    //  ========== www.eXcript.com ==========
    //  ==========                 ==========
    
    int main()
    {
        //scanf("tipo_de_entrada", &variavel_que_recebera);
        //scanf("%tipo1%tipo2", &var1, &var2);
    
        int num1, num2;
        printf("Por favor, digite um numero: \n\n");
        scanf("%i", &num1);
    
        printf("\nO numero digitado foi: %i\n", num1);
    
        return 0;
    }

---
+++ ENTRADA DE DADOS COM C.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+//scanf("tipo_de_entrada", &variavel_que_recebera);
+//scanf("%tipo1%tipo2", &var1, &var2);
+int num1, num2;
+
+printf("digite um numero: \n\n");
+
+scanf("%i", &num1);
+
+printf("\n o numero digitado foi: %i\n", num1);
+
+return 0;
+}
