6747ac4abafbc3692d190fbbd274527633f0e669
JSforBreakfast <veslima3@gmail.com>
2019-07-30 18:18:43 -0300

    Características da Variáveis em C

---
+++ variaveis.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+int i = 1;
+
+char a, b;
+
+a ='1';
+
+b = '2';
+
+printf("%d", i);
+
+return 0;
+
+/*
+
+1)Toda variável possui um nome
+2)Toda variável possui um tipo
+3)Toda variável possui um tamanho
+4)Toda variável possui um valor
+
+a)O armazenamento na memória é temporário
+b)Tudo o que estiver na memória será perdido quando o pc for desligado
+c)Valores armazenados na memória são voláteis
+d)O valor antigo da varável será sobreposto
+
+
+*/
+}
