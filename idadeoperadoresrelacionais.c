a3dfeabe159a4ea4ceb3739a5fad4f593976be59
Vitor Emanuel <veslima3@gmail.com>
2019-08-02 23:59:44 -0300

    Operadores relacionais idade

---
+++ idadeoperadoresrelacionais.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+int main {
+
+    int iJ, iI;
+    iJ = 17;
+    iI = 60;
+
+    int idade = 0;
+
+    printf("Digite a idade de uma pessoa: \n");
+    scanf("%i", &idade);
+
+    if (idade <= iJ){
+
+        printf("n\Diga meu jovem!\n")
+    }else{
+        if (idade >= iI){
+            printf("\nDiga meu nobre!\n")
+            }
+        }
+return 0;
+}
