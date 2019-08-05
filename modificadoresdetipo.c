43c8251c2d83711e8f9e2a22fda7b5f26c18be2b
Vitor Emanuel <veslima3@gmail.com>
2019-08-05 13:59:16 -0300

    Modificadores de tipo

---
+++ modificadoresdetipo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+
+    int i = 10;
+
+    unsigned short int u;
+
+    // short int u;
+
+    // short double u; (not possible)
+
+printf("%i -> variavel i\n", sizeof(i));
+printf("%i -> variavel u\n", sizeof(u));
+
+/*
+
+1)char
+2)int
+3)float
+4)double
+5)void
+
+1)signed    -> + númeors positivos e - nmeros negativos
+2)unsigned  -> + números positivos
+3)long      -> aumentar tamanho do armazenamento da nossa variável
+4)short     -> diminuir tamanho do armazenamento da nossa variável
+
+*/
+
+return 0;
+
+}
